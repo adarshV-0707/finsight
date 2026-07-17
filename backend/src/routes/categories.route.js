71d313fb757f5d73541177ad028eee995a3f378c
Adarsh <vadarsh263@gmail.com>
2026-07-18 03:38:14 +0530

    refactor: expose read-only predefined category route

--- backend/src/routes/categories.route.js
+++ backend/src/routes/categories.route.js
@@ -1,24 +1,12 @@
 import { Router } from "express";
 
-import {
-  createCategory,
-  getCategories,
-  getCategoryById,
-  updateCategory,
-} from "../controllers/category.controller.js";
-
+import { getCategories } from "../controllers/category.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-// All category routes require authentication.
 router.use(verifyJWT);
 
-router.route("/").post(createCategory).get(getCategories);
-
-router
-  .route("/:categoryId")
-  .get(getCategoryById)
-  .patch(updateCategory);
+router.get("/", getCategories);
 
 export default router;
